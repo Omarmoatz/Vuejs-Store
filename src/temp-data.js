b249317df0cb0ffc5ca3db74b6cfdb17248d5e3f
Omarmoatz <omar.raouf.002@gmail.com>
2024-05-02 15:41:52 +0300

    add template data & prepared the project

---
+++ src/temp-data.js
@@ -0,0 +1,39 @@
+import img1 from './assets/1.png'
+import img2 from './assets/2.png'
+import img3 from './assets/3.png'
+import img4 from './assets/4.png'
+import img5 from './assets/5.png'
+
+
+export const tempData = [
+    {
+        id: 1,
+        name: 'T-shirt',
+        image: img1,
+        category: clothes,
+    },
+    {
+        id: 2,
+        name: 'Shoes',
+        image: img2,
+        category: clothes,
+    },
+    {
+        id: 3,
+        name: 'Orange',
+        image: img3,
+        category: fruits,
+    },
+    {
+        id: 4,
+        name: 'tangerine',
+        image: img4,
+        category: fruits,
+    },
+    {
+        id: 5,
+        name: 'brocli',
+        image: img5,
+        category: vegetables,
+    },
+]
---
+++ src/views/ProductView.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <h2>this is product list page</h2>
+</template>
+
+<script >
+
+export default {
+    name: 'ProductView'
+  }
+
+</script>
