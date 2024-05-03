531f39c06f0d33cc745220acffd8b53e65058abb
Omarmoatz <omar.raouf.002@gmail.com>
2024-05-03 04:39:37 +0300

    add detail page design

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,6 +1,32 @@
 <template>
 
-    <h1>This is {{ product.name }} Detail page</h1>
+    <div class="container my-5">
+      <div class="row">
+
+        <div class="col-lg-6">
+          <img :src="product.image" alt="" width="100%"/>
+        </div>
+
+        <div class="col-lg-6 mt-5 border border-1 rounded-3 px-5 py-5">
+          <h1>{{ product.name }}</h1>
+          <div class="d-flex my-3">
+            <h6 class="me-5"><span class="text-primary">brand :</span> {{ product.brand }}</h6>
+            <h6 class=""><span class="text-primary">category :</span> {{ product.category }}</h6>
+          </div>    
+          <p>{{ product.description }}</p>
+          <h4 class="text-success" >$ {{ product.price }}</h4>
+          <!-- add to cart button -->
+          <form class="mt-5" >
+            <div class="d-flex">
+              <input type="text" class="form-control w-25 me-2">
+              <button class="btn btn-success w-100 " >Add to cart</button>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
+    
 
 </template>
 
